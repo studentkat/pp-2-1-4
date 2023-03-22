6a87e86b85caff9b66c6be7c2e85a9e24ea83ff4
studentkat <122557985+studentkat@users.noreply.github.com>
2023-03-22 15:49:21 +0500

    pp-2-1-4

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,12 +1,22 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
 
+
+    //связывание через сеттер
+    private Duck5 duck;
+
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + duck.toString();
+    }
+
+    @Autowired
+    public void setDuck(Duck5 duck) {
+        this.duck = duck;
     }
 }
